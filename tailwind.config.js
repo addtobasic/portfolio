069eeebcfe38aac501065919e9cf5b03ce1f6d17
addtobasic <addtobasic@gmail.com>
2022-01-07 21:00:37 +0900

    :tada: Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          450: '#1ea7fd',
+        },
+      },
+    },
+    fontFamily: {
+      sans: [
+        'Nunito Sans',
+        'Helvetica Neue',
+        'Helvetica',
+        'Arial',
+        'sans-serif',
+      ],
+    },
+    boxShadow: {
+      inner: 'rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
