1888a085805652ad27b8bf4480bfb2c86dcffa10
addtobasic <addtobasic@gmail.com>
2022-03-13 20:15:05 +0900

    :pencil: refs #2 メディアクエリの設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,12 @@ module.exports = {
     boxShadow: {
       inner: 'rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset',
     },
+    screens: {
+      sm: { max: '420px' },
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
   },
   variants: {
     extend: {},
