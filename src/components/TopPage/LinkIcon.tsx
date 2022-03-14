73ffd6e4cf0b584fe8cbc1afa8abeee9d7fe5671
addtobasic <addtobasic@gmail.com>
2022-03-14 16:31:31 +0900

    :art: アイコンの色の修正

--- src/components/TopPage/LinkIcon.tsx
+++ src/components/TopPage/LinkIcon.tsx
@@ -32,7 +32,7 @@ export const LINK_ICON_ITEM = [
 
 export const LinkIcon: React.FC<LinkIconProps> = () => {
   return (
-    <IconContext.Provider value={{ color: 'white', size: '30px' }}>
+    <IconContext.Provider value={{ color: '#ccc', size: '30px' }}>
       <div className='flex max-w-md'>
         <span className='flex-1 px-6 sm:px-2'>
           <a href={LINK_ICON_ITEM[0].url} target='_blank' rel='noreferrer'>
@@ -54,7 +54,6 @@ export const LinkIcon: React.FC<LinkIconProps> = () => {
             <FaInstagram />
           </a>
         </span>
-
         <span className='flex-none px-6 sm:px-2'>
           <a href={LINK_ICON_ITEM[4].url} target='_blank' rel='noreferrer'>
             <FaPenSquare />
--- src/stories/LinkIcon.stories.tsx
+++ src/stories/LinkIcon.stories.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { LinkIcon, LINK_ICON_ITEM } from '../components/TopPage/LinkIcon';
-import { IconContext } from 'react-icons';
-import {
-  FaTwitter,
-  FaGithub,
-  FaFacebook,
-  FaInstagram,
-  FaPenSquare,
-} from 'react-icons/fa';
+import { LinkIcon } from '../components/TopPage/LinkIcon';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -31,37 +23,8 @@ export default {
 } as ComponentMeta<typeof LinkIcon>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof LinkIcon> = () => (
-  <IconContext.Provider value={{ color: '#ccc', size: '30px' }}>
-    <div className='flex max-w-md'>
-      <span className='flex-1 px-6 sm:px-2'>
-        <a href={LINK_ICON_ITEM[0].url} target='_blank' rel='noreferrer'>
-          <FaTwitter />
-        </a>
-      </span>
-      <span className='flex-1 px-6 sm:px-2'>
-        <a href={LINK_ICON_ITEM[1].url} target='_blank' rel='noreferrer'>
-          <FaGithub />
-        </a>
-      </span>
-      <span className='flex-1 px-6 sm:px-2'>
-        <a href={LINK_ICON_ITEM[2].url} target='_blank' rel='noreferrer'>
-          <FaFacebook />
-        </a>
-      </span>
-      <span className='flex-1 px-6 sm:px-2'>
-        <a href={LINK_ICON_ITEM[3].url} target='_blank' rel='noreferrer'>
-          <FaInstagram />
-        </a>
-      </span>
-
-      <span className='flex-none px-6 sm:px-2'>
-        <a href={LINK_ICON_ITEM[4].url} target='_blank' rel='noreferrer'>
-          <FaPenSquare />
-        </a>
-      </span>
-    </div>
-  </IconContext.Provider>
+const Template: ComponentStory<typeof LinkIcon> = (args) => (
+  <LinkIcon {...args} />
 );
 
 export const Primary = Template.bind({});
