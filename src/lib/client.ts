eab90d9da6908bdcc581cf04007b3afbe732a52a
addtobasic <addtobasic@gmail.com>
2022-03-15 17:47:07 +0900

    :pencil: serviceDomainの修正

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
-  serviceDomain: 'cttut9os37',
-  apiKey: process.env.API_KEY || '',
+  serviceDomain: process.env.MICROCMS_SERVICE_ID || '',
+  apiKey: process.env.MICROCMS_API_KEY || '',
 });
