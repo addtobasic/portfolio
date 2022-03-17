63269ceb9e3ec92211aa74f6c568d026c4d5081b
addtobasic <addtobasic@gmail.com>
2022-03-17 17:03:10 +0900

    :pencil: refs #6 設定ファイルの修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,17 +13,11 @@ module.exports = {
     fontFamily: {
       'base-text': ['Roboto'],
     },
-    boxShadow: {
-      inner: 'rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset',
-    },
     screens: {
       sm: { max: '420px' },
       md: '768px',
       lg: '1024px',
       xl: '1280px',
-    },
-    inset: {
-      '1/2': '50%',
     },
   },
   variants: {
