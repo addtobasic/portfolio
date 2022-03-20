d6285545e819f0fcbe505959d2d3016d99fab931
addtobasic <addtobasic@gmail.com>
2022-03-20 16:05:54 +0900

    :sparkles: refs #6 Historyコンポーネントの実装

--- src/components/History/index.tsx
+++ src/components/History/index.tsx
@@ -14,18 +14,25 @@ export const Histories: FC<HistoriesProps> = ({
       <h1 className='flex justify-center py-9 font-base-text text-5xl sm:text-4xl'>
         History
       </h1>
-      <div className=''>
-        {histories.map((history) => (
-          <div key={history.id}>
-            <HistoryCard
-              title={history.title}
-              content={history.content}
-              date={history.date}
-              color={history.color}
-              link={history.link}
-            />
+      <div className='flex justify-center'>
+        <div className=' px-4 pb-10 sm:w-auto md:w-5/6 lg:w-4/6 xl:w-4/6'>
+          <div className='py-7 px-5 bg-white rounded-xl border-2 border-gray-300 shadow-lg'>
+            {histories.map((history) => (
+              <div key={history.id}>
+                <HistoryCard
+                  title={history.title}
+                  content={history.content}
+                  date={history.date}
+                  color={history.color}
+                  link={history.link}
+                />
+              </div>
+            ))}
+            <h5 className='ml-8 font-bold text-gray-600 sm:text-base'>
+              and now...
+            </h5>
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
