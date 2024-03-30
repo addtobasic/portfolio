e62ab9937d6fd4126423887963d98074c9fdd13d
addtobasic <addtobasic@gmail.com>
2024-03-31 04:14:01 +0900

    :pencil: fix: Copyrightのテキストの色の修正

--- src/components/History/Copyright.tsx
+++ src/components/History/Copyright.tsx
@@ -4,7 +4,7 @@ export const Copyright: FC = () => {
   return (
     <div className='py-5'>
       <div className='flex justify-center'>
-        <p className='font-base-text text-base text-white'>
+        <p className='font-base-text text-base text-black'>
           Icon by
           <a href='https://twitter.com/MelvilleTw' className='text-blue-500'>
             @MelvilleTw
@@ -12,7 +12,7 @@ export const Copyright: FC = () => {
         </p>
       </div>
       <div className='flex justify-center'>
-        <p className='font-base-text text-lg text-white'>© 2024 addtobasic</p>
+        <p className='font-base-text text-lg text-black'>© 2024 addtobasic</p>
       </div>
     </div>
   );
